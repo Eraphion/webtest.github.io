4f9aea6a381d774f5e9604b6d5eefb3a4992c22a
Evgeny T <official.trusov.mail@gmail.com>
2018-08-17 00:07:06 +0700

    Added pug template

--- modules/main.js
+++ modules/main.js
@@ -1,1 +1,6 @@
+import header from "./header/script.js";
+
+let pugTemplate = require("./index.pug");
+document.querySelector("#pugTemplate").innerHTML = pugTemplate;
+
 console.log("Hello webpack!");
